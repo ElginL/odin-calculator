9145c7613dddba9ab34f4d176afe3b97d4142560
Elgin <elginlee_2000@hotmail.com>
2021-12-31 19:14:14 +0800

    Add index.html boilerplate, operate function in app.js

--- app.js
+++ app.js
@@ -13,3 +13,18 @@ function mulitiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return mulitiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
+
+console.log(operate("-", 4, 8));
